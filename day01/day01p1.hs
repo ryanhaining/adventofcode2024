10e8ee56d1df121010a380c9efe4c600ca183eb8
Ryan Haining <haining.cpp@gmail.com>
2024-12-01 12:52:40 -0800

    Updates day 1 part 1 haskell with ([Int], [Int])
    Rather than calculating the distance on lists of two elements,
    produces a pair of lists earlier

--- day01/day01p1.hs
+++ day01/day01p1.hs
@@ -3,14 +3,17 @@ import Data.List (sort)
 absDiff :: (Int, Int)-> Int
 absDiff (n1, n2) = abs (n1 - n2)
 
-distance :: [[Int]] -> Int
-distance ns =
-  sum (map absDiff (zip left right))
-  where
-    left = sort (map (!!0) ns)
-    right = sort (map (!!1) ns)
+distance :: ([Int], [Int]) -> Int
+distance (left, right) =
+  sum (map absDiff (zip (sort left) (sort right)))
+
+readPairs :: String -> [Int]
+readPairs = map (read :: String->Int) . words
+
+splitLeftRight :: [[Int]] -> ([Int], [Int])
+splitLeftRight = foldl (\(left, right) [n1, n2] -> (n1:left, n2:right)) ([], [])
 
 main :: IO()
 main = do
   inp <- getContents
-  print $ distance (map  (map (read :: String->Int) . words) (lines inp))
+  print $ distance (splitLeftRight (map readPairs (lines inp)))
