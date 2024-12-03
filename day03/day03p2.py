a58065e6d2734df27216657e5c3bd937b9cb0fc8
Ryan Haining <haining.cpp@gmail.com>
2024-12-02 21:35:24 -0800

    Adds day 3 part 2 python solution

---
+++ day03/day03p2.py
@@ -0,0 +1,14 @@
+import re
+import sys
+
+print(
+    sum(
+        int(n1) * int(n2)
+        for n1, n2 in re.findall(
+            r'mul\((\d+),(\d+)\)',
+            re.sub(
+                r"don't\(\).*?do\(\)", '', sys.stdin.read().replace('\n', ''), count=0
+            ),
+        )
+    )
+)
