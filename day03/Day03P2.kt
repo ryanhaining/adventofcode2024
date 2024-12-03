d49e24c617398778c0d3cc9bf437b4de7564b8b0
Ryan Haining <haining.cpp@gmail.com>
2024-12-02 21:47:44 -0800

    Adds day 3 part 2 kotlin solution
    Not a lot of room for creativity if I'm using regexes here.

---
+++ day03/Day03P2.kt
@@ -0,0 +1,12 @@
+private val MUL = """mul\((\d+),(\d+)\)""".toRegex()
+private val DO_NOT = """don't\(\).*?do\(\)""".toRegex()
+
+fun main() {
+  val contents = generateSequence(::readLine).joinToString("")
+  println(
+      MUL.findAll(contents.replace(DO_NOT, ""))
+      .map { it.groupValues.drop(1).map(String::toInt) }
+      .map { (n1, n2) -> n1 * n2 }
+      .sum()
+  )
+}
