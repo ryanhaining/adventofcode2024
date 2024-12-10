ed66ee2a90fb5e17f15f544c2e343d649c4d9abc
Ryan Haining <haining.cpp@gmail.com>
2024-12-09 18:48:05 -0800

    Rewrites day 9 part 2 from 13s -> 500ms
    Don't actually need to track and move block objects around, just track
    where the blocks would be.

--- day09/day09p2.py
+++ day09/day09p2.py
@@ -1,62 +1,64 @@
+import collections
 from dataclasses import dataclass
 import sys
 from typing import Optional
 
 
+MAX_SIZE = 9
+
+
 @dataclass
 class Block:
-    free: bool
+    index: int
     size: int
-    f_id: Optional[int]
+    f_id: int
 
 
 nums = [int(n) for n in sys.stdin.readlines()[0].rstrip()]
 
+# block size -> list of indices
+free_blocks: collections.defaultdict[int, list[int]] = collections.defaultdict(list)
+
 counter = 0
 fs: list[Block] = []
 free_flag = False
+index = 0
 for n in nums:
     if free_flag:
-        fs.append(Block(free=True, size=n, f_id=None))
+        if n:
+            free_blocks[n].append(index)
     else:
-        fs.append(Block(free=False, size=n, f_id=counter))
+        fs.append(Block(index=index, size=n, f_id=counter))
         counter += 1
+    index += n
     free_flag = not free_flag
 
 
-def find_first_free(size: int, blocks: list[Block]) -> Optional[int]:
-    for i, b in enumerate(blocks):
-        if b.free and b.size >= size:
-            return i
-    return None
+def find_first_free(
+    size: int, free_blocks: dict[int, list[int]]
+) -> Optional[tuple[int, int]]:
+    options = []
+    for s in range(size, MAX_SIZE + 1):
+        if free_blocks[s]:
+            options.append((s, min(free_blocks[s])))
+    return min(options, key=lambda t: t[1]) if options else None
 
 
-def find_block(f_id: int, blocks: list[Block]) -> int:
-    for i, b in enumerate(blocks):
-        if b.f_id == f_id:
-            return i
-    raise ValueError('not found')
-
-
-cur_id = counter
-while cur_id > 0:
-    cur_id -= 1
-    block_index = find_block(cur_id, fs)
-    block = fs[block_index]
-    free_index = find_first_free(block.size, fs)
-    if free_index is None:
+for block in reversed(fs):
+    find_result = find_first_free(block.size, free_blocks)
+    if find_result is None:
         continue
-    free_block = fs[free_index]
-    if free_index < block_index:
-        fs[block_index] = Block(free=True, size=block.size, f_id=None)
-        fs.insert(free_index, block)
-        free_block.size -= block.size
+    free_block_size, free_block_index = find_result
+    if free_block_index > block.index:
+        continue
+    block.index = free_block_index
+    free_blocks[free_block_size].remove(free_block_index)
+    new_free_size = free_block_size - block.size
+    new_free_index = free_block_index + block.size
+    if new_free_size:
+        free_blocks[new_free_size].append(new_free_index)
 
-index = 0
 total = 0
 for b in fs:
-    if not b.free:
-        assert b.f_id is not None
-        total += sum(b.f_id * n for n in range(index, index + b.size))
-    index += b.size
+    total += sum(b.f_id * n for n in range(b.index, b.index + b.size))
 print(total)
